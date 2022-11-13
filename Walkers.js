a3db6fd32394d460542dc9b9f0d5a46f1d858fc4
someonecalleddanny <someonecalleddanny@gmail.com>
2022-11-13 17:45:26 +0000

    zombies colliding

--- Walkers.js
+++ Walkers.js
@@ -21,8 +21,35 @@ class zombie_Man
         //if not hit player == return false;
     }
 
-    lose_health()
+    lose_Health(game)
     {
+        
+        //this.this_Exact_Zombie.setVelocityX(-50).setBounce(1,1);
+        //mySpells.basicSpell.entity.destroy(true);
+        if(spells_Pressed[0] && game.overlap(this.this_Exact_Zombie, mySpells.basicSpell.entity))
+        {
+            game.add.collider(this.this_Exact_Zombie, mySpells.basicSpell.entity);
+            mySpells.basicSpell.entity.destroy(true);
+            console.log("hello");
+        }
+        if(spells_Pressed[1] && game.overlap(this.this_Exact_Zombie, mySpells.fireSpell.entity))
+        {
+            game.add.collider(this.this_Exact_Zombie, mySpells.fireSpell.entity);
+            mySpells.fireSpell.entity.destroy(true);
+            console.log("hello");
+        }
+        if(spells_Pressed[2] && game.overlap(this.this_Exact_Zombie, mySpells.electricSpell.entity))
+        {
+            game.add.collider(this.this_Exact_Zombie, mySpells.electricSpell.entity);
+            mySpells.electricSpell.entity.destroy(true);
+            console.log("hello");
+        }
+        if(spells_Pressed[3] && game.overlap(this.this_Exact_Zombie, mySpells.earthSpell.entity))
+        {
+            game.add.collider(this.this_Exact_Zombie, mySpells.earthSpell.entity);
+            mySpells.earthSpell.entity.destroy(true);
+            console.log("hello");
+        }
 
     }
 }
